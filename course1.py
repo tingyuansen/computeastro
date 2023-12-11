0593c495cc802d041060ed67931dbefa41f9dd76
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2023-12-11 06:08:59 -0600

    Update course1.py

--- course1.py
+++ course1.py
@@ -38,12 +38,6 @@ Upon completing this lab, you should gain expertise in:
 Let's dive in and turn the theoretical knowledge you've acquired into practical skills for tackling real-world data science challenges!
 ''')
 
-st.markdown(r'''Setting up the environment''')
-
-st.code('''import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd''', language='python')
-
 
 
 # Call the app function
