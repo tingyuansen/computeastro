2d8c895b01aefd0f9415bbce76be9dc24c2bc5d4
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2023-12-10 22:48:14 -0600

    Update home.py

--- home.py
+++ home.py
@@ -5,8 +5,8 @@ def app():
     # Page Title
     st.title('A Beginner Guide to Astro-Statistics and Astro-Machine-Learning')
 
-    # Author Information
-    st.markdown('**Author: Yuan-Sen Ting**')
+    # Author Information with Larger Font
+    st.markdown('### **Author: Yuan-Sen Ting**')
 
     # Course Description
     st.write('''
@@ -14,9 +14,9 @@ def app():
 
     In the vast canvas of the universe, deciphering astronomical data is intricately tied to advanced statistical machine learning techniques. Gaining profound insights and enhancing our understanding of the cosmos necessitates a deep dive into methodologies that seamlessly weave together the disciplines of statistics and astronomy.
 
-    As we transition to the latter half of the course, our focus sharpens on the synergy of statistics, machine learning, and astronomy. We prioritize the mathematical, statistical, and computational foundations of these intertwined fields. The curriculum encompasses a range of topics: from Bayesian inference, linear and logistic regression, and dimension reduction techniques to neural networks, sampling, Gaussian Process, and Markov Chain Monte Carlo methods. Time permitting, we will also explore the potential of contemporary large-language models in the realm of astronomical research.
+    We prioritize the mathematical, statistical, and computational foundations of these intertwined fields. The textbook encompasses a range of topics: from Bayesian inference, linear and logistic regression, and dimension reduction techniques to neural networks, sampling, Gaussian Process, and Markov Chain Monte Carlo methods. Time permitting, we will also explore the potential of contemporary large-language models in the realm of astronomical research.
 
-    For all programming-related assignments, participants will utilize Python 3 and the Jupyter notebook, ensuring a hands-on, applied understanding of the concepts discussed.
+    Each chapter also comes with a companion tutorial, where we will utilize Python 3 and the Jupyter notebook, ensuring a hands-on, applied understanding of the concepts discussed.
     ''')
 
     # Additional Course Information
@@ -27,7 +27,9 @@ def app():
 
     # Citation and Bibliography Section
     st.header('Citation and Bibliography')
-    st.write('Details for citing this course and relevant bibliography will be provided here.')
+    st.write('''
+    **Yuan-Sen Ting**. (2023). *A Beginner Guide to Astro-Statistics and Astro-Machine-Learning*. Australia National University. Available at [computeastro.streamlit.app](https://computeastro.streamlit.app/)
+    ''')
 
 # Call the app function
 app()
