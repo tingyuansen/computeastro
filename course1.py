1aefb731f3f473f74e530e8b5be9a71b2f31ce39
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2023-12-11 06:22:19 -0600

    Update course1.py

--- course1.py
+++ course1.py
@@ -45,10 +45,6 @@ Let's dive in and turn the theoretical knowledge you've acquired into practical 
 import numpy as np
 import pandas as pd''', language='python')
 
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import pandas as pd
-
 
 # Call the app function
 app()
