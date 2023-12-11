4efc9e502e9b78686dad0b88c57c7449a8e09a2f
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2023-12-11 06:10:02 -0600

    Update course1.py

--- course1.py
+++ course1.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 def app():
 
-st.markdown(r'''# Linear Regression
+  st.markdown(r'''# Linear Regression
 
-- By Yuan-Sen Ting, August 2023, for ASTR 4004/8004.''')
+  - By Yuan-Sen Ting, August 2023, for ASTR 4004/8004.''')
 
 
 
