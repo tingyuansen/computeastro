923cb5062562b7c6175ff23c30fead610dd5c61c
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2023-12-11 05:52:32 -0600

    Update lecture1_linear_regression.py

--- lecture1_linear_regression.py
+++ lecture1_linear_regression.py
@@ -1,35 +1,56 @@
-# home.py
+# course1.py
 import streamlit as st
 
 def app():
-    # Page Title
-    st.title('Page 2')
+    # Course Page Title
+    st.title('Course 1: Introduction to Astro-Statistics')
 
-    # Author Information with Larger Font
-    st.markdown('### **Author: Yuan-Sen Ting**')
-
-    # Course Description
+    # Introduction Section
+    st.header('Introduction')
     st.write('''
-    A comprehensive textbook delves into essential computational, statistical and data analysis techniques pivotal for daily astronomical research.
-
-    In the vast canvas of the universe, deciphering astronomical data is intricately tied to advanced statistical machine learning techniques. Gaining profound insights and enhancing our understanding of the cosmos necessitates a deep dive into methodologies that seamlessly weave together the disciplines of statistics and astronomy.
-
-    In this textbook, we prioritize the mathematical, statistical, and computational foundations of these intertwined fields. The textbook encompasses a range of topics: from Bayesian inference, linear and logistic regression, and dimension reduction techniques to neural networks, sampling, Gaussian Process, and Markov Chain Monte Carlo methods. 
-
-    Each chapter also comes with a companion tutorial, where we will utilize Python 3 and the Jupyter notebook, ensuring a hands-on, applied understanding of the concepts discussed.
+    Welcome to the first part of our journey into Astro-Statistics and Astro-Machine-Learning. In this section, we will cover the basics of statistical methods as they apply to astronomical data. This will lay the foundation for more advanced topics in later courses.
     ''')
 
-    # Additional Course Information
-    st.info('''
-    This course is developed as my teaching for honour year and Master level course ASTR 4004 / 8004 at the Australia National University.
-    All copyright reserved.
-    ''')
-
-    # Citation and Bibliography Section
-    st.header('How to Cite This Work')
+    # Learning Objectives
+    st.header('Learning Objectives')
     st.write('''
-    This course represents a significant effort on my part to bring advanced concepts in Astro-Statistics and Astro-Machine-Learning to a wider audience. If you find this course useful for your research or teaching, please consider cite this work. Your citation not only recognizes my effort but also helps to disseminate this knowledge to a broader community.
-
-    **Citation Format**:
-    **Yuan-Sen Ting**. (2023). *A Beginner Guide to Astro-Statistics and Astro-Machine-Learning* Available at [computeastro.streamlit.app](https://computeastro.streamlit.app/)
+    By the end of this course, you should be able to:
+    - Understand the basic principles of statistical analysis in astronomy.
+    - Apply fundamental statistical techniques to analyze astronomical data.
+    - Interpret the results of statistical analyses in the context of astronomical research.
     ''')
+
+    # Course Content
+    st.header('Course Content')
+    st.write('''
+    - **Module 1**: Statistical Foundations
+      - Descriptive Statistics
+      - Probability Distributions
+      - Hypothesis Testing
+    - **Module 2**: Applying Statistics to Astronomical Data
+      - Data Collection and Preparation
+      - Statistical Modeling in Astronomy
+      - Case Studies and Practical Examples
+    - **Module 3**: Tools and Techniques
+      - Introduction to Python for Data Analysis
+      - Working with Astronomical Data Sets
+      - Hands-on Exercises and Assignments
+    ''')
+
+    # Additional Resources
+    st.header('Additional Resources')
+    st.write('''
+    Here are some additional resources to help you with this course:
+    - [Link to external resource 1]
+    - [Link to external resource 2]
+    - [Link to external resource 3]
+    ''')
+
+    # Feedback and Questions
+    st.header('Feedback and Questions')
+    st.write('''
+    If you have any questions or feedback about this course, please feel free to reach out. Your input is invaluable in improving this learning experience.
+    ''')
+
+# Call the app function
+app()
