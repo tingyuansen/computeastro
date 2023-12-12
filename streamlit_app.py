6782e02dfe24a7e0db401c124e2f774ac4ca3077
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2023-12-12 10:48:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import lecture9_sampling
 
 def main():
     st.sidebar.title('Course Content')
-    page = st.sidebar.radio("Go to", ["Home", "Chaper 1: Linear Regression", "Chaper 2: Logistic Regression", "Chaper 3: Clustering - K-Means", "Chaper 3: Clustering - Gaussian Mixture Models", "Chaper 4: Dimension Reduction - Principal Component Analysis", "Chaper 9: Sampling"])
+    page = st.sidebar.radio("Go to", ["Home", "Chaper 1: Linear Regression", "Chaper 2: Logistic Regression", "Chaper 3: Clustering - K-Means", "Chaper 3: Clustering - Gaussian Mixture Models", "Chaper 4: Dimension Reduction - Principal Component Analysis", "Chaper 7: Gaussian Process Regression", "Chaper 9: Sampling"])
 
     if page == "Home":
         home.show_page()
@@ -24,6 +24,8 @@ def main():
         lecture3_clustering_gmm.show_page()
     elif page == "Chaper 4: Dimension Reduction - Principal Component Analysis":
         lecture4_pca.show_page()
+    elif page == "Chaper 7: Gaussian Process Regression":
+        lecture7_gaussian_process_regression.show_page()
     elif page == "Chaper 9: Sampling":
         lecture9_sampling.show_page()
         
