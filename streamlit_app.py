4b369ca3445e000e61839144c52ba8d2d77d630a
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2023-12-11 18:50:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,16 @@
 # main_app.py
 import streamlit as st
 import home
-import course1
+import lecture1_linear_regression
 
 def main():
     st.sidebar.title('Course Content')
-    page = st.sidebar.radio("Go to", ["Home", "Course 1"])
-    # page = st.sidebar.selectbox("Choose a page", ["Home", "Course 1"])
+    page = st.sidebar.radio("Go to", ["Home", "Chaper 1: Linear Regression"])
 
     if page == "Home":
         home.show_page()
-    elif page == "Course 1":
-        course1.show_page()
-    elif page == "Course 2":
-        course2.show_page()
+    elif page == "Chaper 1: Linear Regression":
+        lecture1_linear_regression.show_page()
 
 if __name__ == "__main__":
     main()
