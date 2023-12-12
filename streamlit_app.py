c70e6aa510ead847d07427865a15a82acf332779
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2023-12-11 18:49:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,9 @@ import home
 import course1
 
 def main():
-    st.sidebar.title('Navigation')
-    page = st.sidebar.selectbox("Choose a page", ["Home", "Course 1"])
+    st.sidebar.title('Course Content')
+    page = st.sidebar.radio("Go to", ["Home", "Course 1"])
+    # page = st.sidebar.selectbox("Choose a page", ["Home", "Course 1"])
 
     if page == "Home":
         home.show_page()
