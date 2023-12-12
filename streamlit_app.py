59d33aff432d42ecb6e88f4c96a172b6eccb0608
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2023-12-11 21:51:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,11 @@ import home
 import lecture1_linear_regression
 import lecture2_logistic_regression
 import lecture3_clustering_kmeans
+import lecture3_clustering_gmm
 
 def main():
     st.sidebar.title('Course Content')
-    page = st.sidebar.radio("Go to", ["Home", "Chaper 1: Linear Regression", "Chaper 2: Logistic Regression", "Chaper 3: Clustering - K-Means"])
+    page = st.sidebar.radio("Go to", ["Home", "Chaper 1: Linear Regression", "Chaper 2: Logistic Regression", "Chaper 3: Clustering - K-Means", "Chaper 3: Clustering - Gaussian Mixture Models"])
 
     if page == "Home":
         home.show_page()
@@ -17,6 +18,8 @@ def main():
         lecture2_logistic_regression.show_page()
     elif page == "Chaper 3: Clustering - K-Means":
         lecture3_clustering_kmeans.show_page()
+    elif page == "Chaper 3: Clustering - Gaussian Mixture Models":
+        lecture3_clustering_gmm.show_page()
 
 if __name__ == "__main__":
     main()
