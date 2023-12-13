27636ba298a1716c1691c82a2ff6056418a1531a
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2023-12-13 16:03:53 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ import lecture4_pca
 import lecture5_pytorch
 import lecture5_multilayer_perceptron
 import lecture6_autoencoder
+import lecture6_mixed_density_network
 import lecture7_gaussian_process_regression
 import lecture8_gaussian_process_classification
 import lecture9_sampling
@@ -16,7 +17,7 @@ import lecture10_markov_chain_monte_carlo
 
 def main():
     st.sidebar.title('Course Content')
-    page = st.sidebar.radio("Go to", ["Home", "Chaper 1: Linear Regression", "Chaper 2: Logistic Regression", "Chaper 3: Clustering - K-Means", "Chaper 3: Clustering - Gaussian Mixture Models", "Chaper 4: Dimension Reduction - Principal Component Analysis", "Chaper 5: Neural Networks - Pytorch Basics", "Chaper 5: Neural Networks - Multilayer Perceptron", "Chaper 6: Neural Networks - Autoencoder", "Chaper 7: Gaussian Process Regression", "Chaper 8: Gaussian Process Classification", "Chaper 9: Sampling", "Chaper 10: Markov Chain Monte Carlo"])
+    page = st.sidebar.radio("Go to", ["Home", "Chaper 1: Linear Regression", "Chaper 2: Logistic Regression", "Chaper 3: Clustering - K-Means", "Chaper 3: Clustering - Gaussian Mixture Models", "Chaper 4: Dimension Reduction - Principal Component Analysis", "Chaper 5: Neural Networks - Pytorch Basics", "Chaper 5: Neural Networks - Multilayer Perceptron", "Chaper 6: Neural Networks - Autoencoder", "Chaper 6: Neural Networks - Mixed Density Network", "Chaper 7: Gaussian Process Regression", "Chaper 8: Gaussian Process Classification", "Chaper 9: Sampling", "Chaper 10: Markov Chain Monte Carlo"])
 
     if page == "Home":
         home.show_page()
@@ -36,6 +37,8 @@ def main():
         lecture5_multilayer_perceptron.show_page()
     elif page == "Chaper 6: Neural Networks - Autoencoder":
         lecture6_autoencoder.show_page()
+    elif page == "Chaper 6: Neural Networks - Mixed Density Network":
+        lecture6_mixed_density_network.show_page()
     elif page == "Chaper 7: Gaussian Process Regression":
         lecture7_gaussian_process_regression.show_page()
     elif page == "Chaper 8: Gaussian Process Classification":
