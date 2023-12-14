776e0c51995ca89718414f7ec1b6083b15549b1f
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2023-12-13 21:09:15 -0600

    Add files via upload

---
+++ lecture4_pca.py
@@ -0,0 +1,39 @@
+# home.py
+import streamlit as st
+import streamlit.components.v1 as components
+
+def show_page():
+
+    # Embed a YouTube Video
+    video_url = 'https://www.youtube.com/watch?v=YG0C0r21VAw'
+
+    # Page Title
+    st.title('Supervised Learning: Linear Regression')
+
+    # Embed the external HTML page
+    st.info('''
+    Course Slides
+    ''')
+    slideshare_embed_code = """
+        <iframe src="https://www.slideshare.net/slideshow/embed_code/key/l59x3CC7iaHQ2t" 
+                width="700" height="394" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" 
+                style="border:0px solid #000; border-width:0px; margin-bottom:0px; max-width: 100%;" allowfullscreen> 
+        </iframe> 
+    """
+    components.html(slideshare_embed_code, height=412)
+
+    st.markdown('---')
+
+    # Embed a YouTube Video
+    st.info('''
+        Course Video Recording
+    ''')
+    video_embed_code = """
+        <iframe width="700" height="394" src="https://www.youtube.com/embed/YG0C0r21VAw?rel=0&modestbranding=1&autohide=1&showinfo=0&controls=0" frameborder="0" allowfullscreen style="border:none; max-width: 100%;"></iframe>
+    """
+    components.html(video_embed_code, height=412)
+
+
+# This ensures that if someone runs this script directly, it shows the page content.
+if __name__ == "__main__":
+    show_page()
