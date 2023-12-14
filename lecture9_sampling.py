fc0c038d26729c2eac5938ee6225abc250bfb87a
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2023-12-13 21:51:16 -0600

    Update lecture9_sampling.py

--- lecture9_sampling.py
+++ lecture9_sampling.py
@@ -26,7 +26,7 @@ def show_page():
         Course Video Recording
     ''')
     video_embed_code = """
-        <iframe width="700" height="394" src="https://www.youtube.com/watch?v=UuGigcBFItg?rel=0&modestbranding=1&autohide=1&showinfo=0&controls=0" frameborder="0" allowfullscreen style="border:none; max-width: 100%;"></iframe>
+        <iframe width="700" height="394" src="https://www.youtube.com/embed/UuGigcBFItg?rel=0&modestbranding=1&autohide=1&showinfo=0&controls=0" frameborder="0" allowfullscreen style="border:none; max-width: 100%;"></iframe>
     """
     components.html(video_embed_code, height=412)
 
