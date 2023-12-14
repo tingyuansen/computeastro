4dd24afafb54d4c9b620839ed65b87a9d93c29b1
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2023-12-13 21:43:45 -0600

    Update lecture4_pca.py

--- lecture4_pca.py
+++ lecture4_pca.py
@@ -4,9 +4,6 @@ import streamlit.components.v1 as components
 
 def show_page():
 
-    # Embed a YouTube Video
-    video_url = 'https://www.youtube.com/watch?v=YG0C0r21VAw'
-
     # Page Title
     st.title('Unsupervised Learning: Dimension Reduction - Principal Component Analysis')
 
